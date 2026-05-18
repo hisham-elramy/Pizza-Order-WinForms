3d5ad22a758e468bd9fb44c1a1e99f6f6251f431
Hisham <hisham.elramy.dev@gmail.com>
2026-05-18 09:54:23 +0300

    Adding preview folder

--- Pizza App/frmMakeYourPizza.Designer.cs
+++ Pizza App/frmMakeYourPizza.Designer.cs
@@ -426,6 +426,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(871, 516);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnOrder);
